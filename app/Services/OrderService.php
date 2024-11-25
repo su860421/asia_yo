576d588db426cb35b00dd255b2a48a861dc0727e
DESKTOP-VQCEUSC\joe <su860421@gmail.com>
2024-11-25 23:26:55 +0800

    fix:優化order相關邏輯

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -12,23 +12,43 @@ class OrderService implements OrderServiceInterface
 
     public function transform(array $orderData)
     {
-        if (! preg_match('/^[a-zA-Z0-9 ]+$/', $orderData['name'])) {
-            throw new Exception('Name contains non-English characters', 400);
-        } elseif (! ctype_upper(substr($orderData['name'], 0, 1))) {
-            throw new Exception('Name is not capitalized', 400);
-        }
+        $this->validateOrder($orderData);
 
-        if ($orderData['price'] >= 2000) {
-            throw new Exception('Price is over 2000', 400);
-        }
-
-        if (! in_array($orderData['currency'], OrderCurrency::values())) {
-            throw new Exception('Currency format is wrong', 400);
-        } elseif ($orderData['currency'] === OrderCurrency::USD()) {
-            $orderData['price'] *= 31;
+        if ($orderData['currency'] == OrderCurrency::USD()) {
+            $orderData['price'] *= config('setting.usd_to_twd');
             $orderData['currency'] = OrderCurrency::TWD();
         }
 
         return $orderData;
+    }
+
+    private function validateOrder(array $orderData): void
+    {
+        $this->validateName($orderData['name']);
+        $this->validatePrice($orderData['price']);
+        $this->validateCurrency($orderData['currency']);
+    }
+
+    private function validateName(string $name)
+    {
+        if (! preg_match('/^[a-zA-Z0-9 ]+$/', $name)) {
+            throw new Exception('Name contains non-English characters', 400);
+        } elseif (! ctype_upper(substr($name, 0, 1))) {
+            throw new Exception('Name is not capitalized', 400);
+        }
+    }
+
+    private function validatePrice(int $price)
+    {
+        if ($price >= 2000) {
+            throw new Exception('Price is over 2000', 400);
+        }
+    }
+
+    private function validateCurrency(string $currency)
+    {
+        if (! in_array($currency, OrderCurrency::values())) {
+            throw new Exception('Currency format is wrong', 400);
+        }
     }
 }
---
+++ config/setting.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'usd_to_twd' => env('USD_TO_TWD', 31),
+];
